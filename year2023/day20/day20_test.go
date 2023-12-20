37dfa5b8d1f49366477ef80276cb6aba11c2aa98
Christian Persson <saser@users.noreply.github.com>
2023-12-20 10:03:23 +0000

    bootstrap

---
+++ year2023/day20/day20_test.go
@@ -0,0 +1,23 @@
+package day20
+
+import (
+	"testing"
+
+	"go.saser.se/adventofgo/aoctest"
+)
+
+func TestPart1(t *testing.T) {
+	aoctest.Test(t, 2023, 20, 1, Part1)
+}
+
+func TestPart2(t *testing.T) {
+	aoctest.Test(t, 2023, 20, 2, Part2)
+}
+
+func BenchmarkPart1(b *testing.B) {
+	aoctest.Benchmark(b, 2023, 20, 1, Part1)
+}
+
+func BenchmarkPart2(b *testing.B) {
+	aoctest.Benchmark(b, 2023, 20, 2, Part2)
+}
