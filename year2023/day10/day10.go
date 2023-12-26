59d79b8a0e48cd36ad15081a4098c39fef5f0a3c
Christian Persson <saser@users.noreply.github.com>
2023-12-26 23:09:03 +0100

    bootstrap

---
+++ year2023/day10/day10.go
@@ -0,0 +1,18 @@
+package day10
+
+import "errors"
+
+func solve(input string, part int) (string, error) {
+	if part == 2 {
+		return "", errors.New("unimplemented")
+	}
+	return "", errors.New("unimplemented")
+}
+
+func Part1(input string) (string, error) {
+	return solve(input, 1)
+}
+
+func Part2(input string) (string, error) {
+	return solve(input, 2)
+}
