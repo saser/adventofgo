a0ebe10802c3551970530b7d56dcd7a1e13e1534
saser <582273+saser@users.noreply.github.com>
2024-12-19 10:32:26 +0100

    year2024/day19: add solution. (#33)
    This was a fun little problem. Deciding to use memoization was not a planned
    thing, it just felt like something that would help, and it turned out to be very
    effective. Part 1 runs in about 13ms and part 2 in about 27ms on my laptop.

---
+++ year2024/day19/day19.go
@@ -0,0 +1,77 @@
+package day19
+
+import (
+	"fmt"
+	"strings"
+
+	"go.saser.se/adventofgo/striter"
+)
+
+// arrangementCount returns the number of possible arrangements for the given
+// towel.
+func arrangementCount(towel string, patterns []string, memo map[string]uint64) (n uint64) {
+	if prev, ok := memo[towel]; ok {
+		return prev
+	}
+	defer func() { memo[towel] = n }()
+	if towel == "" {
+		return 1
+	}
+	var sum uint64
+	for _, p := range patterns {
+		if strings.HasPrefix(towel, p) {
+			sum += arrangementCount(strings.TrimPrefix(towel, p), patterns, memo)
+		}
+	}
+	return sum
+}
+
+// isPossible returns 1 if the given towel is possible to create, and 0
+// otherwise.
+func isPossible(towel string, patterns []string, memo map[string]uint64) (n uint64) {
+	if prev, ok := memo[towel]; ok {
+		return prev
+	}
+	defer func() { memo[towel] = n }()
+	if towel == "" {
+		return 1
+	}
+	for _, p := range patterns {
+		if strings.HasPrefix(towel, p) && isPossible(strings.TrimPrefix(towel, p), patterns, memo) == 1 {
+			return 1
+		}
+	}
+	return 0
+}
+
+func solve(input string, part int) (string, error) {
+	lines := striter.OverLines(input)
+	i := 0
+	var patterns []string
+	memo := make(map[string]uint64)
+	var answer uint64
+	countFn := isPossible
+	if part == 2 {
+		countFn = arrangementCount
+	}
+	for line, ok := lines.Next(); ok; line, ok = lines.Next() {
+		switch i {
+		case 0:
+			patterns = strings.Split(line, ", ")
+		case 1:
+			// Empty line between patterns and towels.
+		default:
+			answer += countFn(line, patterns, memo)
+		}
+		i++
+	}
+	return fmt.Sprint(answer), nil
+}
+
+func Part1(input string) (string, error) {
+	return solve(input, 1)
+}
+
+func Part2(input string) (string, error) {
+	return solve(input, 2)
+}
