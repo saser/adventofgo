8f6b1307ae89bcd8ca86aaee7195f23e81ef0802
Christian Persson <582273+saser@users.noreply.github.com>
2024-12-28 18:06:15 +0100

    year2024/day11: add solution.
    Maybe there are more efficient memoization mechanisms (for example, there are
    definitely cycles such as 0 -> 1 -> 2024 -> 2,>>0<<,2,4 -> 1 -> 2024 -> ...),
    but this is fast enough and pretty simple.

---
+++ year2024/day11/day11.go
@@ -0,0 +1,77 @@
+package day11
+
+import (
+	"fmt"
+	"strconv"
+
+	"go.saser.se/adventofgo/striter"
+)
+
+func digits(v uint64) uint64 {
+	var n uint64 = 0
+	for v != 0 {
+		n++
+		v /= 10
+	}
+	return n
+}
+
+func blink2(stones []uint64, blinks int) uint64 {
+	type key struct {
+		Stone     uint64
+		Remaining int
+	}
+	memo := make(map[key]uint64)
+	var blinkAux func(stone uint64, blinks int) uint64
+	blinkAux = func(stone uint64, blinks int) (n uint64) {
+		k := key{Stone: stone, Remaining: blinks}
+		if prev, seen := memo[k]; seen {
+			return prev
+		}
+		defer func() { memo[k] = n }()
+		if blinks == 0 {
+			return 1
+		}
+		if stone == 0 {
+			return blinkAux(1, blinks-1)
+		}
+		if n := digits(stone); n%2 == 0 {
+			var p uint64 = 1
+			for range n / 2 {
+				p *= 10
+			}
+			return blinkAux(stone/p, blinks-1) + blinkAux(stone%p, blinks-1)
+		}
+		return blinkAux(stone*2024, blinks-1)
+	}
+	var sum uint64 = 0
+	for _, s := range stones {
+		sum += blinkAux(s, blinks)
+	}
+	return sum
+}
+
+func solve(input string, part int) (string, error) {
+	var stones []uint64
+	fields := striter.OverSplit(input, " ")
+	for field, ok := fields.Next(); ok; field, ok = fields.Next() {
+		n, err := strconv.ParseUint(field, 10, 64)
+		if err != nil {
+			return "", err
+		}
+		stones = append(stones, n)
+	}
+	blinks := 25
+	if part == 2 {
+		blinks = 75
+	}
+	return fmt.Sprint(blink2(stones, blinks)), nil
+}
+
+func Part1(input string) (string, error) {
+	return solve(input, 1)
+}
+
+func Part2(input string) (string, error) {
+	return solve(input, 2)
+}
