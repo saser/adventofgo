e7e6ef34cdd2583895ea5ec3a6e20ff0494e091d
saser <582273+saser@users.noreply.github.com>
2024-12-29 01:25:02 +0100

    year2024/day23: add solution. (#43)
    This was a really fun problem, I almost always like graph problems. I didn't
    come up with the final solution myself (but I did come up with _a_ solution
    myself), I just searched the web for algorithms that find cliques. It turns out
    that choosing the "with pivoting" version of the Bron-Kerbosch algorithm
    improved the runtime by ~15x (i.e. a decrease of ~94%) compared to the "without
    pivoting" version, from ~300ms to ~20ms -- a massive difference from a few lines
    of code!

---
+++ year2024/day23/day23.go
@@ -0,0 +1,118 @@
+package day23
+
+import (
+	"cmp"
+	"fmt"
+	"slices"
+	"strings"
+
+	"go.saser.se/adventofgo/container/set"
+	"go.saser.se/adventofgo/striter"
+)
+
+func parse(input string) (map[string]*set.Set[string], error) {
+	lines := striter.OverLines(input)
+	edges := make(map[string]*set.Set[string])
+	for line, ok := lines.Next(); ok; line, ok = lines.Next() {
+		from, to, ok := strings.Cut(line, "-")
+		if !ok {
+			return nil, fmt.Errorf("malformed line: %q", line)
+		}
+		if _, ok := edges[from]; !ok {
+			edges[from] = set.New[string]()
+		}
+		edges[from].Add(to)
+		if _, ok := edges[to]; !ok {
+			edges[to] = set.New[string]()
+		}
+		edges[to].Add(from)
+	}
+	return edges, nil
+}
+
+func Part1(input string) (string, error) {
+	edges, err := parse(input)
+	if err != nil {
+		return "", err
+	}
+	type component struct{ N1, N2, N3 string }
+	seen := set.New[component]()
+	answer := 0
+	for n1 := range edges {
+		for n2 := range edges[n1].All() {
+			for n3 := range edges[n2].All() {
+				connected := edges[n1].Contains(n3)
+				hasT := n1[0] == 't' || n2[0] == 't' || n3[0] == 't'
+				if !(connected && hasT) {
+					continue
+				}
+				nodes := []string{n1, n2, n3}
+				slices.Sort(nodes)
+				c := component{
+					N1: nodes[0],
+					N2: nodes[1],
+					N3: nodes[2],
+				}
+				if seen.Contains(c) {
+					continue
+				}
+				seen.Add(c)
+				answer++
+			}
+		}
+	}
+	return fmt.Sprint(answer), nil
+}
+
+// runBronKerboschWithPivot implements the algorithm described at
+// https://en.wikipedia.org/wiki/Bron%E2%80%93Kerbosch_algorithm#With_pivoting
+// retrieved on 2024-12-29. The method for choosing the pivot element is to just
+// pick a random element in P ⋃ X. I experimented with picking the element with
+// the maximum degree, but that didn't have any meaningful impact on my runtime.
+func runBronKerboschWithPivot(edges map[string]*set.Set[string], r, p, x *set.Set[string], report func(*set.Set[string])) {
+	if p.Len() == 0 && x.Len() == 0 {
+		report(r)
+		return
+	}
+	var u string
+	for n := range set.Union(p, x).All() {
+		u = n
+		break
+	}
+	nu := edges[u]
+	for v := range set.Minus(p, nu).All() {
+		nv := edges[v]
+		runBronKerboschWithPivot(edges, set.Union(r, set.Of(v)), set.Intersection(p, nv), set.Intersection(x, nv), report)
+		p.Delete(v)
+		x.Add(v)
+	}
+}
+
+// maximalCliques runs the Bron-Kerbosch algorithm
+// (https://en.wikipedia.org/wiki/Bron%E2%80%93Kerbosch_algorithm) to gather a
+// list of all the maximal cliques. The maximum clique will be the largest
+// clique in this list.
+func maximalCliques(edges map[string]*set.Set[string]) []*set.Set[string] {
+	var cliques []*set.Set[string]
+	report := func(clique *set.Set[string]) {
+		cliques = append(cliques, clique.Clone())
+	}
+	r := set.New[string]()
+	p := set.NewSize[string](len(edges))
+	for n := range edges {
+		p.Add(n)
+	}
+	x := set.New[string]()
+	runBronKerboschWithPivot(edges, r, p, x, report)
+	return cliques
+}
+
+func Part2(input string) (string, error) {
+	edges, err := parse(input)
+	if err != nil {
+		return "", err
+	}
+	cliques := maximalCliques(edges)
+	largest := slices.MaxFunc(cliques, func(a, b *set.Set[string]) int { return cmp.Compare(a.Len(), b.Len()) })
+	return strings.Join(slices.Sorted(largest.All()), ","), nil
+}
