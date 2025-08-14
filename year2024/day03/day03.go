2a7355eef36cec861ff7bcc9b3fd094dec97ab7c
Christian Persson <582273+saser@users.noreply.github.com>
2025-08-14 22:22:21 +0200

    remove old code

--- year2024/day03/day03.go
+++ year2024/day03/day03.go
@@ -2,10 +2,7 @@ package day03
 
 import (
 	"fmt"
-	"regexp"
 )
-
-var instructionRE = regexp.MustCompile(`(?:do\(\)|don't\(\)|mul\((\d{1,3}),(\d{1,3})\))`)
 
 type parser struct {
 	input string
@@ -117,8 +114,6 @@ func (p *parser) sum2() int {
 }
 
 func solve(input string, part int) (string, error) {
-	// input = `xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))`
-	// input = `xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))`
 	p := &parser{input: input}
 	var sum int
 	if part == 1 {
@@ -127,43 +122,12 @@ func solve(input string, part int) (string, error) {
 		sum = p.sum2()
 	}
 	return fmt.Sprint(sum), nil
-	// matches := instructionRE.FindAllStringSubmatch(input, -1)
-	// sum := 0
-	// enabled := true
-	// for _, submatches := range matches {
-	// 	if s := submatches[0]; s == "do()" || s == "don't()" {
-	// 		if part == 1 {
-	// 			continue
-	// 		}
-	// 		if s == "do()" {
-	// 			enabled = true
-	// 			continue
-	// 		}
-	// 		if s == "don't()" {
-	// 			enabled = false
-	// 			continue
-	// 		}
-	// 	}
-	// 	if !enabled {
-	// 		continue
-	// 	}
-	// 	a, err := strconv.Atoi(submatches[1])
-	// 	if err != nil {
-	// 		return "", err
-	// 	}
-	// 	b, err := strconv.Atoi(submatches[2])
-	// 	if err != nil {
-	// 		return "", err
-	// 	}
-	// 	sum += a * b
-	// }
-	// return fmt.Sprint(sum), nil
 }
 
 func Part1(input string) (string, error) {
-	return solve(input, 1)
+	return fmt.Sprint((&parser{input: input}).sum1()), nil
 }
 
 func Part2(input string) (string, error) {
-	return solve(input, 2)
+	return fmt.Sprint((&parser{input: input}).sum2()), nil
 }
