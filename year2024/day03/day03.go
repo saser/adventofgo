b10d063b098901b98ff0cc10b807701db64c4d67
Christian Persson <582273+saser@users.noreply.github.com>
2025-10-31 20:34:09 +0100

    year2024/day03: significant optimizations. (#47)
    I realized that while regexes are easy, they are not *that* fast. I realized
    that the "langauge" is so simple that just hand-rolling a simple parser instead
    was probably going to run much faster. Turns out that it does. The benchmarks
    speak for themselves:
    
        $ benchstat base.txt opt3.txt
        goos: linux
        goarch: amd64
        pkg: go.saser.se/adventofgo/year2024/day03
        cpu: AMD Ryzen 9 5900HX with Radeon Graphics
                 │   base.txt    │              opt3.txt               │
                 │    sec/op     │   sec/op     vs base                │
        Part1-16   1163.15µ ± 1%   18.88µ ± 2%  -98.38% (p=0.000 n=10)
        Part2-16   1156.23µ ± 1%   25.25µ ± 1%  -97.82% (p=0.000 n=10)
        geomean      1.160m        21.83µ       -98.12%
    
                 │    base.txt    │              opt3.txt              │
                 │      B/op      │    B/op     vs base                │
        Part1-16   137139.50 ± 0%   24.00 ± 0%  -99.98% (p=0.000 n=10)
        Part2-16   137122.50 ± 0%   16.00 ± 0%  -99.99% (p=0.000 n=10)
        geomean      133.9Ki        19.60       -99.99%
    
                 │   base.txt    │              opt3.txt              │
                 │   allocs/op   │ allocs/op   vs base                │
        Part1-16   1618.000 ± 0%   2.000 ± 0%  -99.88% (p=0.000 n=10)
        Part2-16   1618.000 ± 0%   2.000 ± 0%  -99.88% (p=0.000 n=10)
        geomean      1.618k        2.000       -99.88%

--- year2024/day03/day03.go
+++ year2024/day03/day03.go
@@ -2,42 +2,130 @@ package day03
 
 import (
 	"fmt"
-	"regexp"
-	"strconv"
 )
 
-var instructionRE = regexp.MustCompile(`(?:do\(\)|don't\(\)|mul\((\d{1,3}),(\d{1,3})\))`)
+type parser struct {
+	input string
+	pos   int
+}
 
-func solve(input string, part int) (string, error) {
-	matches := instructionRE.FindAllStringSubmatch(input, -1)
-	sum := 0
-	enabled := true
-	for _, submatches := range matches {
-		if s := submatches[0]; s == "do()" || s == "don't()" {
-			if part == 1 {
-				continue
-			}
-			if s == "do()" {
-				enabled = true
-				continue
-			}
-			if s == "don't()" {
-				enabled = false
-				continue
-			}
+func (p *parser) literal(lit string) bool {
+	for i, c := range []byte(lit) {
+		if p.input[p.pos+i] != c {
+			return false
 		}
-		if !enabled {
+	}
+	p.pos += len(lit)
+	return true
+}
+
+func (p *parser) number() (int, bool) {
+	n := 0
+	digits := 0
+	for range 3 {
+		c := p.input[p.pos]
+		if c < '0' || c > '9' {
+			break
+		}
+		p.pos++
+		digits++
+		n = n*10 + int(c-'0')
+	}
+	return n, digits > 0
+}
+
+func (p *parser) sum1() int {
+	sum := 0
+	for p.pos < len(p.input) {
+		// We can achieve significant speedups by simply continuing early if
+		// there's no chance that the current position is the start of one of
+		// the tokens we care about.
+		if p.input[p.pos] != 'm' {
+			p.pos++
 			continue
 		}
-		a, err := strconv.Atoi(submatches[1])
-		if err != nil {
-			return "", err
+		if !p.literal("mul(") {
+			p.pos++
+			continue
 		}
-		b, err := strconv.Atoi(submatches[2])
-		if err != nil {
-			return "", err
+		a, ok := p.number()
+		if !ok {
+			p.pos++
+			continue
+		}
+		if !p.literal(",") {
+			p.pos++
+			continue
+		}
+		b, ok := p.number()
+		if !ok {
+			p.pos++
+			continue
+		}
+		if !p.literal(")") {
+			p.pos++
+			continue
 		}
 		sum += a * b
+	}
+	return sum
+}
+
+func (p *parser) sum2() int {
+	sum := 0
+	enabled := true
+	for p.pos < len(p.input) {
+		// We can achieve significant speedups by simply continuing early if
+		// there's no chance that the current position is the start of one of
+		// the tokens we care about.
+		if c := p.input[p.pos]; c != 'd' && c != 'm' {
+			p.pos++
+			continue
+		}
+		if p.literal("do()") {
+			enabled = true
+			continue
+		}
+		if p.literal("don't()") {
+			enabled = false
+			continue
+		}
+		if !p.literal("mul(") {
+			p.pos++
+			continue
+		}
+		a, ok := p.number()
+		if !ok {
+			p.pos++
+			continue
+		}
+		if !p.literal(",") {
+			p.pos++
+			continue
+		}
+		b, ok := p.number()
+		if !ok {
+			p.pos++
+			continue
+		}
+		if !p.literal(")") {
+			p.pos++
+			continue
+		}
+		if enabled {
+			sum += a * b
+		}
+	}
+	return sum
+}
+
+func solve(input string, part int) (string, error) {
+	p := &parser{input: input}
+	var sum int
+	if part == 1 {
+		sum = p.sum1()
+	} else {
+		sum = p.sum2()
 	}
 	return fmt.Sprint(sum), nil
 }
