14d1d72df3d8102ffc504971ba8e7fa2496ad320
Christian Persson <christian.eg.persson@gmail.com>
2025-11-15 00:16:56 +0100

    year2024/day16: add solution. (#53)
    This runs in a few hundred milliseconds on my laptop, which I'm not super happy
    about. My intuition, partially confirmed by looking at profiles, is that my
    solution spends a lot of time/memory copying slices all around the place. To
    improve on this I think it's possible to use a "came from" map to retrace the
    steps, rather than carrying around the steps for each single queued state. For
    part 2, that map would have to be multi-valued (so instead of mapping
    `cameFrom[dst] = src`, it would map `cameFrom[dst] = []src` ). However, I don't
    have the patience or energy to implement this right now. Maybe in the future.

---
+++ year2024/day16/day16_test.go
@@ -0,0 +1,23 @@
+package day16
+
+import (
+	"testing"
+
+	"go.saser.se/adventofgo/aoctest"
+)
+
+func TestPart1(t *testing.T) {
+	aoctest.Test(t, 2024, 16, 1, Part1)
+}
+
+func TestPart2(t *testing.T) {
+	aoctest.Test(t, 2024, 16, 2, Part2)
+}
+
+func BenchmarkPart1(b *testing.B) {
+	aoctest.Benchmark(b, 2024, 16, 1, Part1)
+}
+
+func BenchmarkPart2(b *testing.B) {
+	aoctest.Benchmark(b, 2024, 16, 2, Part2)
+}
