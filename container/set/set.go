32037d900919aa5d61dc7280868093cf1cbe0358
Christian Persson <582273+saser@users.noreply.github.com>
2025-11-15 00:53:12 +0100

    container/set: change representation and simplify APIs.
    Might as well.

--- container/set/set.go
+++ container/set/set.go
@@ -8,24 +8,11 @@ import (
 
 // Set represents an unsorted set of values. It is not thread-safe; use together
 // with e.g. a sync.Mutex to ensure thread-safety.
-type Set[T comparable] struct {
-	m map[T]struct{}
-}
-
-// New initializes a new set.
-func New[T comparable]() *Set[T] {
-	return &Set[T]{m: make(map[T]struct{})}
-}
-
-// NewSize initializes a new set that has allocted enough memory to hold n
-// elements.
-func NewSize[T comparable](n int) *Set[T] {
-	return &Set[T]{m: make(map[T]struct{}, n)}
-}
+type Set[T comparable] map[T]struct{}
 
 // Of initializes a set that holds the given elements.
-func Of[T comparable](vs ...T) *Set[T] {
-	s := NewSize[T](len(vs))
+func Of[T comparable](vs ...T) Set[T] {
+	s := make(Set[T], (len(vs)))
 	for _, v := range vs {
 		s.Add(v)
 	}
@@ -33,8 +20,8 @@ func Of[T comparable](vs ...T) *Set[T] {
 }
 
 // Union returns a new set holding a ⋃ b.
-func Union[T comparable](a, b *Set[T]) *Set[T] {
-	c := NewSize[T](a.Len() + b.Len())
+func Union[T comparable](a, b Set[T]) Set[T] {
+	c := make(Set[T], a.Len()+b.Len())
 	for v := range a.All() {
 		c.Add(v)
 	}
@@ -45,8 +32,8 @@ func Union[T comparable](a, b *Set[T]) *Set[T] {
 }
 
 // Intersection returns a new set holding a ⋂ b.
-func Intersection[T comparable](a, b *Set[T]) *Set[T] {
-	c := NewSize[T](max(a.Len(), b.Len()))
+func Intersection[T comparable](a, b Set[T]) Set[T] {
+	c := make(Set[T], max(a.Len(), b.Len()))
 	for v := range a.All() {
 		if b.Contains(v) {
 			c.Add(v)
@@ -56,7 +43,7 @@ func Intersection[T comparable](a, b *Set[T]) *Set[T] {
 }
 
 // Minus returns a new set holding a \ b.
-func Minus[T comparable](a, b *Set[T]) *Set[T] {
+func Minus[T comparable](a, b Set[T]) Set[T] {
 	c := a.Clone()
 	for v := range b.All() {
 		c.Delete(v)
@@ -65,43 +52,37 @@ func Minus[T comparable](a, b *Set[T]) *Set[T] {
 }
 
 // Clone returns a copy of s.
-func (s *Set[T]) Clone() *Set[T] {
-	return &Set[T]{m: maps.Clone(s.m)}
+func (s Set[T]) Clone() Set[T] {
+	return maps.Clone(s)
 }
 
 // Add ensures v is contained in s, and returns true if s was modified.
-func (s *Set[T]) Add(v T) bool {
+func (s Set[T]) Add(v T) bool {
 	added := !s.Contains(v)
-	s.m[v] = struct{}{}
+	s[v] = struct{}{}
 	return added
 }
 
 // Delete ensures v is not contained in s, and returns true if s was modified.
-func (s *Set[T]) Delete(v T) bool {
+func (s Set[T]) Delete(v T) bool {
 	deleted := s.Contains(v)
-	delete(s.m, v)
+	delete(s, v)
 	return deleted
 }
 
 // Contains returns true if s contains v.
-func (s *Set[T]) Contains(v T) bool {
-	_, ok := s.m[v]
+func (s Set[T]) Contains(v T) bool {
+	_, ok := s[v]
 	return ok
 }
 
 // Len returns the number of elements in the set.
-func (s *Set[T]) Len() int {
-	return len(s.m)
+func (s Set[T]) Len() int {
+	return len(s)
 }
 
 // All iterates over the elements in the set in an undefined, non-deterministic
 // order.
-func (s *Set[T]) All() iter.Seq[T] {
-	return func(yield func(T) bool) {
-		for v := range s.m {
-			if !yield(v) {
-				return
-			}
-		}
-	}
+func (s Set[T]) All() iter.Seq[T] {
+	return maps.Keys(s)
 }
--- year2024/day23/day23.go
+++ year2024/day23/day23.go
@@ -9,19 +9,19 @@ import (
 	"go.saser.se/adventofgo/container/set"
 )
 
-func parse(input string) (map[string]*set.Set[string], error) {
-	edges := make(map[string]*set.Set[string])
+func parse(input string) (map[string]set.Set[string], error) {
+	edges := make(map[string]set.Set[string])
 	for line := range strings.SplitSeq(input, "\n") {
 		from, to, ok := strings.Cut(line, "-")
 		if !ok {
 			return nil, fmt.Errorf("malformed line: %q", line)
 		}
 		if _, ok := edges[from]; !ok {
-			edges[from] = set.New[string]()
+			edges[from] = make(set.Set[string])
 		}
 		edges[from].Add(to)
 		if _, ok := edges[to]; !ok {
-			edges[to] = set.New[string]()
+			edges[to] = make(set.Set[string])
 		}
 		edges[to].Add(from)
 	}
@@ -34,7 +34,7 @@ func Part1(input string) (string, error) {
 		return "", err
 	}
 	type component struct{ N1, N2, N3 string }
-	seen := set.New[component]()
+	seen := make(set.Set[component])
 	answer := 0
 	for n1 := range edges {
 		for n2 := range edges[n1].All() {
@@ -67,7 +67,7 @@ func Part1(input string) (string, error) {
 // retrieved on 2024-12-29. The method for choosing the pivot element is to just
 // pick a random element in P ⋃ X. I experimented with picking the element with
 // the maximum degree, but that didn't have any meaningful impact on my runtime.
-func runBronKerboschWithPivot(edges map[string]*set.Set[string], r, p, x *set.Set[string], report func(*set.Set[string])) {
+func runBronKerboschWithPivot(edges map[string]set.Set[string], r, p, x set.Set[string], report func(set.Set[string])) {
 	if p.Len() == 0 && x.Len() == 0 {
 		report(r)
 		return
@@ -90,17 +90,17 @@ func runBronKerboschWithPivot(edges map[string]*set.Set[string], r, p, x *set.Se
 // (https://en.wikipedia.org/wiki/Bron%E2%80%93Kerbosch_algorithm) to gather a
 // list of all the maximal cliques. The maximum clique will be the largest
 // clique in this list.
-func maximalCliques(edges map[string]*set.Set[string]) []*set.Set[string] {
-	var cliques []*set.Set[string]
-	report := func(clique *set.Set[string]) {
+func maximalCliques(edges map[string]set.Set[string]) []set.Set[string] {
+	var cliques []set.Set[string]
+	report := func(clique set.Set[string]) {
 		cliques = append(cliques, clique.Clone())
 	}
-	r := set.New[string]()
-	p := set.NewSize[string](len(edges))
+	r := make(set.Set[string])
+	p := make(set.Set[string], len(edges))
 	for n := range edges {
 		p.Add(n)
 	}
-	x := set.New[string]()
+	x := make(set.Set[string])
 	runBronKerboschWithPivot(edges, r, p, x, report)
 	return cliques
 }
@@ -111,6 +111,6 @@ func Part2(input string) (string, error) {
 		return "", err
 	}
 	cliques := maximalCliques(edges)
-	largest := slices.MaxFunc(cliques, func(a, b *set.Set[string]) int { return cmp.Compare(a.Len(), b.Len()) })
+	largest := slices.MaxFunc(cliques, func(a, b set.Set[string]) int { return cmp.Compare(a.Len(), b.Len()) })
 	return strings.Join(slices.Sorted(largest.All()), ","), nil
 }
